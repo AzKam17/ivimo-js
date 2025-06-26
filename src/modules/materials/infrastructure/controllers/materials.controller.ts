ffd0c5155200217ad556f319c3b59069fa4bc88b
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-06-26 09:01:12 +0000

    feat(materials): add basic materials controller with GET endpoint

--- src/modules/materials/infrastructure/controllers/materials.controller.ts
+++ src/modules/materials/infrastructure/controllers/materials.controller.ts
@@ -1,3 +1,11 @@
 import Elysia from "elysia";
 
-export const MaterialsController = new Elysia()
+export const MaterialsController = new Elysia({ prefix: "/materials" })
+    .get("/", async () => {
+        return "Hello materials";
+    }, {
+        detail: {
+            tags: ['Materials']
+        }
+    })
+;
